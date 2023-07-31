aa1e708b99e31a072ef961fe20f0584fd526ac64
zakaria-boughaba <93131771+zakaria-boughaba@users.noreply.github.com>
2023-08-01 00:33:47 +0100

    Add more skills and description

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import SocialIcons from '../SocialIcons/SocialIcons';
 import './Footer.css'
 
 const Footer = () => {
   return (
-    <footer id="footer" className='container-md'>
-        <hr className="line" style={{ width: '100%' }} />
-          <p className="copyrights ">All Rights Reserved &copy;2023-2024 | by <span>Boughaba Zakaria</span></p>
+    <footer id="footer" className='my-4 container-md'>
+      <p className="copyrights ">All Rights Reserved &copy;2023-2024 | by <span>Boughaba Zakaria</span></p>
     </footer>
   );
 }
